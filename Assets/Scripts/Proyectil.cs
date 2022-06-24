68eefe3993591f6a9726d533e74fc958b8402500
Panmartinez627 <107763381+Panmartinez627@users.noreply.github.com>
2022-06-24 20:08:05 -0300

    V 0.0.4

--- Assets/Scripts/Proyectil.cs
+++ Assets/Scripts/Proyectil.cs
@@ -2,6 +2,9 @@
 
 public class Proyectil : MonoBehaviour
 {
+    public int speed = 20;
+    public Vector3 movimientoBala = new Vector3(0.1f,0,0);
+    public int damage = 25;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +14,6 @@ public class Proyectil : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0.1f,0,0);
+        transform.position += (movimientoBala * speed * Time.deltaTime);
     }
 }
